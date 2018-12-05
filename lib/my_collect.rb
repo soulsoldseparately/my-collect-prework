b311b79fbd143538d017a5faab7358641a644e7c
Daniel Toma <danieltoma@ubalumni.com>
2018-12-05 03:58:15 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(array)
+  if block_given?
+    counter = 0
+    while counter < array.length
+      yield(array[counter])
+      counter += 1
+    end
+    
+  else
+    "No block given"
+  end
+end
